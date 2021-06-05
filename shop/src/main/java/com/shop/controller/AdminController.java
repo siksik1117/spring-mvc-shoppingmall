53c9a068c3466922b2dc1141e3eebba9071c6135
siksik1117 <rla1gus1tlr@naver.com>
2021-06-05 16:47:34 +0900

    상품 카테고리 구성

--- shop/src/main/java/com/shop/controller/AdminController.java
+++ shop/src/main/java/com/shop/controller/AdminController.java
@@ -1,20 +1,44 @@
 package com.shop.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.shop.domain.CategoryVO;
+import com.shop.service.AdminService;
+
+import net.sf.json.JSONArray;
 
 @Controller
 @RequestMapping("/admin/*")
 public class AdminController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);			
+	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);	
+	
+	@Inject
+	AdminService adminService;
+	
 	//관리자 화면
 	@RequestMapping(value="/index", method = RequestMethod.GET)
 	public void getIndex() throws Exception {
 		logger.info("get index");
+	}
+	
+	//상품 등록
+	@RequestMapping(value="/goods/register", method = RequestMethod.GET)
+	public void getGoodsRegister(Model model) throws Exception {
+		logger.info("get goods register");
+		
+		List<CategoryVO> category = null;
+		category = adminService.category();
+		model.addAttribute("category", JSONArray.fromObject(category));
 	}
 
 }
